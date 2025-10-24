cb076f1c8b82b01a1534bdf2403b500abb015cd7
Timona-tt <tinaaghaali2@gmail.com>
2025-10-24 16:31:16 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
   });
 });
+// منوی کشویی در موبایل
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+  dropdown.addEventListener('click', function(e) {
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      this.classList.toggle('active');
+    }
+  });
+});
